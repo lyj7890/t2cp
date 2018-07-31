1678eab6bd0edf8747a3ab82965010a23b5086f1
liyijie <liyijie@sinorail.com>
2018-07-31 03:30:53 -0400

    add jenkins ci cd

---
+++ jenkins_ci_cd/staging/config.js
@@ -0,0 +1,17 @@
+'use strict';
+
+(function() {
+  var masterPublicHostname = 'http://192.168.131.108:30009';
+
+  window.CONFIG = {
+    publicCloud: true,
+    // ..................
+    api: {
+      login: masterPublicHostname + '/api/v1',
+      console: masterPublicHostname + '/portal',
+      admin: masterPublicHostname + '/portal',
+      paas: masterPublicHostname + '/api/v1'
+    }
+  }
+})();
+
